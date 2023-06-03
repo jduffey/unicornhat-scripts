171ea19da2286a40d39b7997cbaeb56e6bc5278d
Jed Duffey <jedduffey@gmail.com>
2023-06-03 16:06:35 -0400

    Parametrize tests to cover inputs 0 through 10

--- test_eth_base_fee.py
+++ test_eth_base_fee.py
@@ -2,20 +2,18 @@ import pytest
 from eth_base_fee import calculate_rgb
 
 
-def test_calculate_rgb():
-    r, g, b = calculate_rgb(0)
-    assert r == 0
-    assert g == 0
-    assert b == 255
-
-    y = 2
-    r, g, b = calculate_rgb(y)
-    assert r == 0
-    assert g == int((y/3) * 255)
-    assert b == 255 - int((y/3) * 255)
-
-    y = 5
-    r, g, b = calculate_rgb(y)
-    assert r == 255
-    assert g == 255 - int(((y-4)/3) * 255)
-    assert b == 0
+@pytest.mark.parametrize("y, expected", [
+    (0, (0, 0, 255)),
+    (1, (0, int((1/3) * 255), 255 - int((1/3) * 255))),
+    (2, (0, int((2/3) * 255), 255 - int((2/3) * 255))),
+    (3, (0, 255, 0)),
+    (4, (255, 255, 0)),
+    (5, (255, 255 - int((1/3) * 255), 0)),
+    (6, (255, 255 - int((2/3) * 255), 0)),
+    (7, (255, 0, 0)),
+    (8, (255, -85, 0)),
+    (9, (255, -170, 0)),
+    (10, (255, -255, 0)),
+])
+def test_calculate_rgb(y, expected):
+    assert calculate_rgb(y) == expected
